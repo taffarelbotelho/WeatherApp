ce95141684ed6d0cf31e2e183af39bb39dd5a1e2
taffarel <taffarel.botelho1@gmail.com>
2024-10-02 22:17:11 -0300

    blank initial project

---
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.weather
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.weather.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+
+    }
+}
